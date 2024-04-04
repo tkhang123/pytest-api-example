17eb5a8e44bf9727a7936c66dd90d2677ca5fbf8
Thay Khang <tkhang123@gmail.com>
2024-04-04 07:28:58 -0400

    Coding updates

--- test_pet.py
+++ test_pet.py
@@ -5,11 +5,12 @@ import api_helpers
 from hamcrest import assert_that, contains_string, is_
 
 '''
-TODO: Finish this test by...
+TODO: FIXED: schemas.py had "integer" instead of "string" for "name.type"
 1) Troubleshooting and fixing the test failure
 The purpose of this test is to validate the response matches the expected schema defined in schemas.py
 '''
 def test_pet_schema():
+    # TODO: Should probably add a check here to make sure the id exists?
     test_endpoint = "/pets/1"
 
     response = api_helpers.get_api_data(test_endpoint)
@@ -26,7 +27,8 @@ TODO: Finish this test by...
 3) Validate the 'status' property in the response is equal to the expected status
 4) Validate the schema for each object in the response
 '''
-@pytest.mark.parametrize("status", [("available")])
+# TODO: 1) Extending the parameterization to include all available statuses
+@pytest.mark.parametrize("status", [("available"), ("pending"), ("sold")])
 def test_find_by_status_200(status):
     test_endpoint = "/pets/findByStatus"
     params = {
@@ -34,13 +36,90 @@ def test_find_by_status_200(status):
     }
 
     response = api_helpers.get_api_data(test_endpoint, params)
-    # TODO...
+    # TODO: 2) Validate the appropriate response code
+    assert response.status_code == 200
+
+    pets = response.json()
+    for pet in pets:
+        # TODO: 3) Validate the 'status' property in the response is equal to the expected status
+        assert params["status"] == pet["status"]
+        # TODO: 4) Validate the schema for each object in the response
+        validate(instance=pet, schema=schemas.pet)
+
 
 '''
 TODO: Finish this test by...
 1) Testing and validating the appropriate 404 response for /pets/{pet_id}
 2) Parameterizing the test for any edge cases
 '''
-def test_get_by_id_404():
-    # TODO...
-    pass
+# TODO: 2) Parameterizing the test for any edge cases, since schema says "integer", tried -1 and marked it as xfail
+@pytest.mark.parametrize("id, expected_code", [(0, 200),                                            # expected success
+                                               (5, 404),                                            # expected failure
+                                               pytest.param(-1, 200, marks=pytest.mark.xfail),      # expected sucess, but needs investigation
+                                               pytest.param("A", 404, marks=pytest.mark.xfail)])    # expected failure, but needs investigation
+def test_get_by_id_404(id, expected_code):
+    # TODO: Should probably add a check here to make sure the id exists?
+    test_endpoint = "/pets/" + str(id)
+
+    response = api_helpers.get_api_data(test_endpoint)
+    print(response.json())
+    # TODO: 1) Validate the appropriate response code
+    assert response.status_code == expected_code
+
+
+# OTHER BUGS:
+# 1) Was able to create duplicate pet ids with a string like "0", backend needs to validate the is input is a integer
+# [
+#   {
+#     "id": 0,
+#     "name": "snowball",
+#     "type": "cat",
+#     "status": "available"
+#   },
+#   {
+#     "id": 1,
+#     "name": "ranger",
+#     "type": "dog",
+#     "status": "pending"
+#   },
+#   {
+#     "id": 2,
+#     "name": "flippy",
+#     "type": "fish",
+#     "status": "available"
+#   },
+#   {
+#     "id": 0,
+#     "name": "test2",
+#     "type": "cat",
+#     "status": "available"
+#   }
+# ]
+# 2) Was able to create pet with negative integers like "-1", backend needs to validate the positive integers only
+# [
+#   {
+#     "id": 0,
+#     "name": "snowball",
+#     "type": "cat",
+#     "status": "available"
+#   },
+#   {
+#     "id": 1,
+#     "name": "ranger",
+#     "type": "dog",
+#     "status": "pending"
+#   },
+#   {
+#     "id": 2,
+#     "name": "flippy",
+#     "type": "fish",
+#     "status": "available"
+#   },
+#   {
+#     "id": -1,
+#     "name": "test2",
+#     "type": "cat",
+#     "status": "available"
+#   }
+# ]
+#3) The Order model "id" should probably be an integer.
--- test_store.py
+++ test_store.py
@@ -1,4 +1,5 @@
 from jsonschema import validate
+import random
 import pytest
 import schemas
 import api_helpers
@@ -12,5 +13,64 @@ TODO: Finish this test by...
 3) Validate the response codes and values
 4) Validate the response message "Order and pet status updated successfully"
 '''
-def test_patch_order_by_id():
-    pass
+# TODO: 2) *Optional* Consider using @pytest.fixture to create unique test data for each run
+# Function to create a new pet with a random pet_id
+@pytest.fixture
+def create_new_pet():
+    endpoint = "/pets/"
+    rand_id = random.randrange(10000, 20000)
+    payload = {
+        "id": str(rand_id),
+        "name": "test_" + str(rand_id),
+        "type": "cat",
+        "status": "available"
+    }
+
+    post_response = api_helpers.post_api_data(endpoint, data=payload)
+    return post_response.json()["id"]
+
+# Function to create an order using the new_pet_id and return the uuid
+def get_order_id(new_pet_id):
+    # time.sleep(5)
+    post_order_endpoint = "/store/order"
+    post_order_payload = {
+        "pet_id": str(new_pet_id)
+    }
+
+    post_response = api_helpers.post_api_data(post_order_endpoint, data=post_order_payload)
+    return post_response.json()["id"]
+
+# PATCH the "status" of an order.  Had to create a new pet to get a unique pet_id and order_id since there is no api
+# to get the order_id.  Using parametrized varibles to test multiple iterations of status and expected status_code. 
+@pytest.mark.parametrize("status, expected_code", [("available", 200), 
+                                                   ("pending", 200), 
+                                                   ("sold", 200), 
+                                                   ("invalid status", 400), 
+                                                   ("invalid order", 404) ])
+def test_patch_order_by_id(create_new_pet, status, expected_code):
+    if (status == "available" or status == "sold" or status == "pending"):
+        id = get_order_id(create_new_pet)
+        stat = status
+        expected_message = "Order and pet status updated successfully"
+    elif status == "invalid status":
+        id = get_order_id(create_new_pet)
+        stat = "invalid status"
+        expected_message = "Invalid status 'invalid status'. Valid statuses are available, sold, pending"
+    else:
+        id = "invalid"
+        stat = status
+        expected_message = 'Order not found. You have requested this URI [/store/order/invalid] but did you mean /store/order/<string:order_id> or /store/order ?'
+    # Create an unique order_id and it's endpoint
+    test_endpoint = "/store/order/" + id
+    payload = {
+        "status": stat
+    }
+
+    # Send PATCH request with payload
+    response = api_helpers.patch_api_data(test_endpoint, data=payload)
+
+    # TODO: 3) Validate the response codes and values
+    assert response.status_code == expected_code
+    # TODO: 4) Validate the response message "Order and pet status updated successfully"
+    print(response.json())
+    assert response.json()["message"] == expected_message
